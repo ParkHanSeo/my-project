91c2b69c26dfed395a1ec5e6bd878c7c211071f7
ParkJeeHoon <gkstj8300@naver.com>
2024-04-04 17:55:53 +0900

    style: 단순 css 수정

--- src/pages/intro/index.tsx
+++ src/pages/intro/index.tsx
@@ -1,18 +1,13 @@
 import { NextPage } from 'next';
 import styles from './Intro.module.scss';
+import { FirstSection } from './FirstSection/FirstSection';
+import { SecondSection } from './SecondSection/SecondSection';
 
 const Intro: NextPage = () => {
     return (
         <div>
-            <section className={styles.firstSection}>
-                <div className={styles.firstSectionWrap}>
-                    <div className={styles.textWrap}>
-                        <h1>내 일상속에 한줄기</h1>
-                        <h3>마음의 편안함</h3>
-                        <p className="siar">SIAR</p>
-                    </div>
-                </div>
-            </section>
+            <FirstSection />
+            <SecondSection />
         </div>
     )
 }
