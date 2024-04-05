99ea66d660211547898397669858cddcf3e865f8
ParkJeeHoon <gkstj8300@naver.com>
2024-04-05 10:35:33 +0900

    feat: SecondSection 생성 완료

--- src/pages/intro/SecondSection/SecondSection.tsx
+++ src/pages/intro/SecondSection/SecondSection.tsx
@@ -10,8 +10,26 @@ export const SecondSection = () => {
                         <br />
                         나아가는 독서 플랫폼
                     </h2>
+                    <p>
+                        마음의 교양과 편안함을
+                        <br />
+                        함께 드리겠습니다
+                    </p>
                 </div>
-                <div className={styles.listWrap}></div>
+                <div className={styles.listWrap}>
+                    <ul className={styles.cardList}>
+                        <li>
+                            <div className={styles.cardTitle}>
+                                <strong>내가 찾던 도서를 한눈에</strong>
+                            </div>
+                            <p className={styles.cardSubTitle}>
+                                Temp 내용 1
+                                <br />
+                                Temp 내용 2
+                            </p>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </section>
     )
