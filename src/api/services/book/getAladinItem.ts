d9384eb7be2285dc4f52c538656997344803a2c8
ParkJeeHoon <gkstj8300@naver.com>
2024-04-11 16:45:29 +0900

    feat: BookDetail 컴포넌트 생성

--- src/api/services/book/getAladinItem.ts
+++ src/api/services/book/getAladinItem.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { AladinItemReqeust } from "@/models/api/book/AladinItemReqeust";
 import { AladinItemResponse } from "@/models/api/book/AladinItemListResponse";
+import { error } from "console";
 
 export async function getAladinItem (
     reqeust: AladinItemReqeust
@@ -15,5 +16,10 @@ export async function getAladinItem (
         TTBKey: process.env.NEXT_PUBLIC_ALADIN_TTBKEY,
         ItemId: reqeust.ItemId
     }
-    return data.get('/getBook', { params: req });
+    try {
+        const res = await data.get('/getBook', { params: req });
+        return res.data.item[0];
+    } catch (error) {
+        throw error;
+    }
 }
--- src/components/BookDetail/BookDetail.tsx
+++ src/components/BookDetail/BookDetail.tsx
@@ -8,8 +8,22 @@ type Props = {
 export const BookDetail: React.FC<Props> = ({
     book
 }) => {
-
+    console.log(book)
     return (
-        <div>Check</div>
+        <div className={styles.detail}>
+            <div className={styles.contents}>
+                <div className={styles.coverContainer}>
+                    <div className={styles.thombnail}>
+                        <div className={styles.inner}>
+                            <div className={styles.imageWrap}>
+                                <picture>
+                                    <img src={book.cover} alt={book.title}/>
+                                </picture>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
